49734b89d0226423417e0690cc553bbb51e37cac
Lag <f.gete@ludus-academie.com>
2019-11-15 10:38:33 +0100

    Pascal Exo 1
    Pascal Exo 1

---
+++ exo1.pas
@@ -0,0 +1,13 @@
+program exo1;
+
+uses crt;
+var val, double : integer;
+
+BEGIN
+        val:=231;
+        double:=(val*2);
+        writeln(val);
+        writeln(double);
+        readln;
+END.
+
